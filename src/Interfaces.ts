35bc9cffcd8a05cd1894e74ec42aecba59675839
Cata <11278743+cataclym@users.noreply.github.com>
2023-01-08 20:25:10 +0100

    Split Classes and interfaces. Added remaining endpoint methods

---
+++ src/Interfaces.ts
@@ -0,0 +1,69 @@
+export interface Artist {
+    id: string
+    name: string
+    url?: string
+    images?: number
+}
+
+export interface _Source {
+    name?: string
+    url?: string
+}
+
+export interface Category {
+    id: string;
+    name: string;
+    description: string;
+    nsfw: boolean;
+    type: string;
+    createdAt: Date;
+}
+
+export interface Character {
+    id: string;
+    name: string;
+    description: string;
+    createdAt: Date;
+}
+
+export interface Dimensions {
+    width: number,
+    height: number,
+    orientation: ImageOrientation,
+}
+
+export interface NekosResponse {
+    data: NekosData[];
+    success: boolean;
+}
+
+export interface NekosData {
+    id: string;
+    url: string;
+    artist: null;
+    source: _Source;
+    original: null;
+    nsfw: string;
+    categories: Category[];
+    characters: any[];
+    createdAt: Date;
+    meta: Meta;
+}
+
+export interface Meta {
+    eTag: string;
+    size: number;
+    mimetype: string;
+    color: string;
+    expires: Date;
+    dimens: Dimensions;
+}
+
+export interface NekosCategoryResponse {
+    data: Category[];
+    success: boolean;
+}
+
+export type ImageOrientation = "landscape" | "portrait" | "square";
+export type HexString = `#${string}`;
+export type NsfwLevel = null | "sfw" | "questionable" | "nsfw";
--- src/NekosAPI.ts
+++ src/NekosAPI.ts
@@ -1,4 +1,8 @@
-import fetch, { RequestInfo, RequestInit, Response } from 'node-fetch';
+import fetch, { RequestInfo, RequestInit, Response } from "node-fetch";
+import { URL, URLSearchParams } from "url";
+import { Artist, Category, Character, NekosCategoryResponse, NekosResponse } from "./Interfaces";
+import NekosImage from "./NekosImage";
+import isRedirect = fetch.isRedirect;
 
 export class NekosAPI {
 
@@ -9,23 +13,26 @@ export class NekosAPI {
         if (token && NekosAPI.validateToken(token)) {
             this.token = token;
         }
-        this.baseUrl = 'https://nekos.nekidev.com/api/';
+        this.baseUrl = "https://nekos.nekidev.com/api/";
     }
 
     public async getImages(limit = 10, offset = 0): Promise<NekosImage[]> {
 
         if (!this.token) throw new Error("You need a valid access token to use this method.")
 
-        if (limit > 25) limit = 25;
-
         const options = {
             method: "POST",
             headers: {
-                "Authorization": `Bearer ${this.token}`
+                "Authorization": `Bearer ${this.token}`,
             },
         };
 
-        const response = await fetch(`${this.baseUrl}image?limit=${limit}&offset=${offset}`, options);
+        const url = new URL(`${this.baseUrl}image?`);
+
+        url.searchParams.append("offset", String(offset));
+        url.searchParams.append("limit", String(limit));
+
+        const response = await fetch(url, options);
 
         await NekosAPI.checkResponseCode(response);
 
@@ -33,128 +40,105 @@ export class NekosAPI {
             .map(image => new NekosImage(image));
     }
 
-    public async getRandomImage(): Promise<NekosImage> {
-        const response = await fetch(`${this.baseUrl}image/random`);
+    public async getRandomImage(categories?: string | string[], limit?: number): Promise<NekosImage[]> {
+
+        const query = categories
+            ? Array.isArray(categories)
+                ? categories.join(",")
+                : categories || ""
+            : "";
+
+        const limitQuery = limit || 1;
+
+        const url = new URL(`${this.baseUrl}image/random?`);
+
+        url.searchParams.append("categories", query);
+        url.searchParams.append("limit", String(limitQuery));
+
+        const response = await fetch(url);
 
         await NekosAPI.checkResponseCode(response);
 
-        return new NekosImage((<NekosResponse> await response.json())["data"][0]);
+        return (<NekosResponse> await response.json())["data"]
+            .map(image => new NekosImage(image));
+    }
+
+    public async getImageByID(id: string): Promise<NekosImage> {
+
+        const url = new URL(`${this.baseUrl}image/${id}`);
+
+        const response = await fetch(url);
+
+        await NekosAPI.checkResponseCode(response)
+
+        return new NekosImage((await response.json())["data"]);
+    }
+
+    public async getArtistByID(id: string): Promise<Artist> {
+
+        const url = new URL(`${this.baseUrl}artist/${id}`);
+
+        const response = await fetch(url);
+
+        await NekosAPI.checkResponseCode(response)
+
+        return (await response.json())["data"] as Artist;
+    }
+
+    public async getImagesByArtistID(id: string, limit = 10, offset = 0): Promise<NekosImage[]> {
+
+        const url = new URL(`${this.baseUrl}artist/${id}/images?`);
+
+        url.searchParams.append("limit", String(limit));
+        url.searchParams.append("offset", String(offset));
+
+        const response = await fetch(url);
+
+        await NekosAPI.checkResponseCode(response)
+
+        return (<NekosResponse> await response.json())["data"]
+            .map(image => new NekosImage(image));
+    }
+
+    public async getCharacterByID(id: string): Promise<Character> {
+
+        const url = new URL(`${this.baseUrl}character/${id}`);
+
+        const response = await fetch(url);
+
+        await NekosAPI.checkResponseCode(response)
+
+        return (await response.json())["data"] as Character;
+    }
+
+    public async getCategory(limit = 10, offset = 0): Promise<Category[]> {
+
+        const url = new URL(`${this.baseUrl}category?`);
+
+        url.searchParams.append("limit", String(limit));
+        url.searchParams.append("offset", String(offset));
+
+        const response = await fetch(url);
+
+        await NekosAPI.checkResponseCode(response)
+
+        return (<NekosCategoryResponse> await response.json())["data"];
     }
 
     private static validateToken(token?: string) {
         if ((token && Array.from(token.matchAll(NekosAPI.tokenRegex)).length) || !token) {
-            throw new Error('The token is invalid. It should be 100 characters long and contain numbers and lowercase/uppercase characters.');
-        } else {
+            throw new Error("The token is invalid. It should be 100 characters long and contain numbers and lowercase/uppercase characters.");
+        }
+        else {
             return true;
         }
     }
 
     private static async checkResponseCode(response: Response) {
         if ((response.status > 200 && response.status <= 300) || !response.ok) {
-            throw new Error(`An error occurred while fetching the data from the server. ${response.statusText} ${response.status}`)
+            throw new Error(`An error occurred while fetching the data from the server. ${response.statusText}. Status: ${response.status}`)
         }
     }
 
     private static tokenRegex = /^[0-9a-zA-Z]{100}$/;
 }
-
-class NekosImage {
-    readonly id: string;
-    readonly url: string;
-    readonly artist: Artist | null;
-    readonly source: _Source;
-    readonly original: boolean | null;
-    readonly nsfw: NsfwLevel;
-    readonly categories: Category[];
-    readonly characters: Characters[];
-    readonly createdAt: Date;
-    readonly etag: string;
-    readonly size: number;
-    readonly mimetype: string;
-    readonly color: HexString;
-    readonly expires: any;
-    readonly dimensions: Dimensions;
-    constructor(data: NekosData) {
-        this.id=data["id"];
-        this.url=data["url"];
-        this.artist=data["artist"];
-        this.source=data["source"];
-        this.original=data["original"];
-        this.nsfw=data["nsfw"] as NsfwLevel;
-        this.categories=data["categories"];
-        this.characters=data["characters"];
-        this.createdAt=new Date(data["createdAt"]);
-        this.etag=data["meta"]["eTag"];
-        this.size=data["meta"]["size"];
-        this.mimetype=data["meta"]["mimetype"];
-        this.color=data["meta"]["color"] as HexString;
-        this.expires=data["meta"]["expires"];
-        this.dimensions=data["meta"]["dimens"]
-    }
-}
-
-type NsfwLevel = null | "sfw" | "questionable" | "nsfw";
-
-interface Artist {
-    id: string
-    name: string
-    url?: string
-    images?: number
-}
-
-interface _Source {
-    name?: string
-    url?: string
-}
-
-interface Category {
-    id:          string;
-    name:        string;
-    description: string;
-    nsfw:        boolean;
-    type:        string;
-    createdAt:   Date;
-}
-
-interface Characters {
-    id: string;
-    name: string;
-    description: string;
-    createdAt: Date;
-}
-
-interface Dimensions {
-    width: number,
-    height: number,
-    orientation: ImageOrientation,
-}
-
-export interface NekosResponse {
-    data:    NekosData[];
-    success: boolean;
-}
-
-export interface NekosData {
-    id:         string;
-    url:        string;
-    artist:     null;
-    source:     _Source;
-    original:   null;
-    nsfw:       string;
-    categories: Category[];
-    characters: any[];
-    createdAt:  Date;
-    meta:       Meta;
-}
-
-export interface Meta {
-    eTag:     string;
-    size:     number;
-    mimetype: string;
-    color:    string;
-    expires:  Date;
-    dimens:   Dimensions;
-}
-
-type ImageOrientation = "landscape" | "portrait" | "square";
-type HexString = `#${string}`;
